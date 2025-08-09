26e972c4a6a83777f3db33a3296ec2c2a956164c
saifeemaimoona <saifeemaimoona@gmail.com>
2025-08-09 16:04:02 +0530

    mobile version comleted, animations fixed

---
+++ script_timeline.js
@@ -0,0 +1,47 @@
+gsap.to("#red",{
+    rotate:360,
+    duration:8,
+    repeat:-1,
+    ease:'none'
+})
+gsap.from("#green",{
+    rotate:360,
+    duration:8,
+    repeat:-1,
+    ease:'none'
+})
+gsap.to(".purple",{
+    rotate:360,
+    duration:5,
+    repeat:-1,
+    ease:'none'
+})
+
+
+// document.querySelectorAll('.card1').forEach(card => {
+//     card.addEventListener('click', () => {
+//         document.querySelectorAll('.card1').forEach(c => {
+//             if (c !== card) c.classList.remove('active');
+//         });
+//         card.classList.toggle('active');
+//     });
+// });
+document.querySelectorAll('.card1').forEach(card => {
+  const extra = card.querySelector('.extra');
+
+  card.addEventListener('click', () => {
+    const isActive = card.classList.contains('active');
+
+    // Close all cards
+    document.querySelectorAll('.card1').forEach(c => {
+      c.classList.remove('active');
+      c.querySelector('.extra').style.maxHeight = null;
+    });
+
+    // If the clicked one was not active, open it
+    if (!isActive) {
+      card.classList.add('active');
+      extra.style.maxHeight = extra.scrollHeight + "px";
+    }
+  });
+});
