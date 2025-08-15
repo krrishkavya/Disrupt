1f37f029c447c916034e768eeba2a60e0b061479
Deependra Singh <deependras@iitg.ac.in>
2025-08-15 19:37:59 +0530

    Update script_speaker.js

--- static/js/script_speaker.js
+++ static/js/script_speaker.js
@@ -1,11 +1,16 @@
 cardsData =[
-    ["Mr. Narayan Murthy","Co-Founder, Infosys","/images/logo.png"],
-    ["Mr. Narayan Murthy","Co-Founder, Infosys","/images/logo.png"],
-    ["Mr. Narayan Murthy","Co-Founder, Infosys","/images/logo.png"],
-    ["Mr. Narayan Murthy","Co-Founder, Infosys","/images/logo.png"],
-    ["Mr. Narayan Murthy","Co-Founder, Infosys","/images/logo.png"],
-    ["Mr. Narayan Murthy","Co-Founder, Infosys","/images/logo.png"],
-
+    ["Mr. Ashneer Grover","Co-Founder, Bharatpe","/images/logo.png"],
+    ["Mr. Narayan Murthy","Founder, Infosys","/images/logo.png"],
+    ["Mr. Krishnan V","Co-Founder, Justdial","/images/logo.png"],
+    ["Mr. Ankush Singla","Co-Founder, Coding Ninja","/images/logo.png"],
+    ["Mr. Charlie Cheever","Co-Founder, Quora","/images/logo.png"],
+    ["Mr. Jimmy Wales","Founder, Wikipedia","/images/logo.png"],
+    ["Mr. Pulkit Jain","Co-Founder, Vedantu","/images/logo.png"],
+    ["Mr. Kevin Harrington","Original Shark, Shark Tank","/images/logo.png"],
+    ["Mr. Michael Siebal","CEO, Y Combinator","/images/logo.png"],
+    ["Mr. Rohit Kapoor","CEO, Food Marketplace Swiggy","/images/logo.png"],
+    ["Mr. Ankit Nagori","Founder, Curefit","/images/logo.png"],
+    ["Mr. Amod Malviya","Co-Founder, Udaan","/images/logo.png"],
 ]
 function addCard([name,company,url]){
     main = document.querySelector('.speaker-main');
